8f9abde655ff17ab6be7014a9357b03443b29ee4
firdevs <firdevsgzr4@gmail.com>
2023-05-14 18:54:59 +0300

    Bir dosyanın hash çıktısını alma
    Bu çalışmada ilk olarak  C# dili kullanılarak bir ara yüz oluşturulmuş ve bir dosyanın hash çıktısını almak için sha-256 algoritması kullanılmıştır. Daha sonra mevcut dosyamınız hash çıktısı ile başka bir hash çıktısının karşılaştırılabileceği bir bölüm de arayüze eklenmiştir.

---
+++ Hash Hesaplama Odev/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.txtSHA256Hash = new System.Windows.Forms.TextBox();
+            this.btnVerifyHash = new System.Windows.Forms.Button();
+            this.txtUserHash = new System.Windows.Forms.TextBox();
+            this.lblVerificationResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(82, 112);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(197, 88);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Dosya Seç";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(121, 232);
+            this.txtFilePath.Multiline = true;
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(539, 26);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // txtSHA256Hash
+            // 
+            this.txtSHA256Hash.Location = new System.Drawing.Point(22, 296);
+            this.txtSHA256Hash.Multiline = true;
+            this.txtSHA256Hash.Name = "txtSHA256Hash";
+            this.txtSHA256Hash.Size = new System.Drawing.Size(755, 30);
+            this.txtSHA256Hash.TabIndex = 2;
+            // 
+            // btnVerifyHash
+            // 
+            this.btnVerifyHash.Location = new System.Drawing.Point(528, 112);
+            this.btnVerifyHash.Name = "btnVerifyHash";
+            this.btnVerifyHash.Size = new System.Drawing.Size(179, 88);
+            this.btnVerifyHash.TabIndex = 3;
+            this.btnVerifyHash.Text = "Hash Doğrulama";
+            this.btnVerifyHash.UseVisualStyleBackColor = true;
+            this.btnVerifyHash.Click += new System.EventHandler(this.btnVerifyHash_Click);
+            // 
+            // txtUserHash
+            // 
+            this.txtUserHash.Location = new System.Drawing.Point(22, 375);
+            this.txtUserHash.Multiline = true;
+            this.txtUserHash.Name = "txtUserHash";
+            this.txtUserHash.Size = new System.Drawing.Size(755, 29);
+            this.txtUserHash.TabIndex = 4;
+            // 
+            // lblVerificationResult
+            // 
+            this.lblVerificationResult.AutoSize = true;
+            this.lblVerificationResult.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblVerificationResult.Location = new System.Drawing.Point(339, 62);
+            this.lblVerificationResult.Name = "lblVerificationResult";
+            this.lblVerificationResult.Size = new System.Drawing.Size(133, 13);
+            this.lblVerificationResult.TabIndex = 5;
+            this.lblVerificationResult.Text = "Hash Karşılaştırma Sonucu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblVerificationResult);
+            this.Controls.Add(this.txtUserHash);
+            this.Controls.Add(this.btnVerifyHash);
+            this.Controls.Add(this.txtSHA256Hash);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.TextBox txtSHA256Hash;
+        private System.Windows.Forms.Button btnVerifyHash;
+        private System.Windows.Forms.TextBox txtUserHash;
+        private System.Windows.Forms.Label lblVerificationResult;
+    }
+}
+
---
+++ Hash Hesaplama Odev/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "All Files (*.*)|*.*";
+                openFileDialog.Title = "Dosya Seç";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtFilePath.Text = openFileDialog.FileName;
+
+                    byte[] hashValue = ComputeSHA256Checksum(openFileDialog.FileName);
+
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < hashValue.Length; i++)
+                    {
+                        sb.Append(hashValue[i].ToString("X2")); // hexadecimal format
+                    }
+
+                    txtSHA256Hash.Text = sb.ToString();
+                }
+            }
+        }
+
+        private byte[] ComputeSHA256Checksum(string fileName)
+        {
+            using (SHA256 SHA256 = SHA256Managed.Create())
+            {
+                using (FileStream fileStream = File.OpenRead(fileName))
+                {
+                    return SHA256.ComputeHash(fileStream);
+                }
+            }
+        }
+
+        private void btnVerifyHash_Click(object sender, EventArgs e)
+        {
+            if (txtSHA256Hash.Text == txtUserHash.Text)
+            {
+                lblVerificationResult.Text = "Hash doğrulandı!";
+            }
+            else
+            {
+                lblVerificationResult.Text = "Hash doğrulanamadı!";
+            }
+        }
+    }
+}
